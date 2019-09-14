9992aeeac97fe44fe2930d1507c97fb1dc361001
akhilchow9999 <54537917+akhilchow9999@users.noreply.github.com>
2019-09-14 16:21:45 +0530

    MyCode

---
+++ GreatOutdoorsAkhil/GreatOutdoors.DataAccesslayer/Bag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatOutdoors.DataAccesslayer
+{
+    public class Bag
+    {
+        public double CartValue;
+        Product product = new Product(); //Product object
+
+        public List<int> cart = new List<int>(); //cart with product id's
+
+        public void AddToCart(int productId, int addToCartQuantity)
+        {
+
+            cart.Add(productId);
+
+
+            CartValue += (product.Price * addToCartQuantity);
+        }
+
+        public void RemoveFromCart(int productId, int removeFromCartQuantity)
+        {
+
+            cart.Remove(productId);
+
+            CartValue -= (product.Price * removeFromCartQuantity);
+        }
+
+        public int TotalQuantityOrdered(int addToCartQuantity, int removeFromCartQuantity)
+        {
+            return (addToCartQuantity - removeFromCartQuantity);
+        }
+    }
+}
---
+++ GreatOutdoorsAkhil/GreatOutdoors.Entities/GreatOutdoorsEntities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatOutdoors.Entities
+{
+    public class Order
+    {
+        private static int _orderId = 0;
+        private DateTime _dateOfOrder;
+
+        public static int OrderId { get => _orderId; set => _orderId = value; }
+
+        public DateTime DateOfOrder { get => _dateOfOrder; set => _dateOfOrder = value; }
+    }
+}
